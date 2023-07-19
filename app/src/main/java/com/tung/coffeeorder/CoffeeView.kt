cd253003f9390334db37ad35cd9bf7303f628d7d
viettungvuong <viettungvuong@gmail.com>
2023-07-19 22:48:04 +0700

    Code cho coffee view

--- app/src/main/java/com/tung/coffeeorder/CoffeeView.kt
+++ app/src/main/java/com/tung/coffeeorder/CoffeeView.kt
@@ -4,16 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.tung.coffeeorder.Functions.Companion.reformatNumber
 
 class CoffeeView(coffee: Coffee): Fragment() {
+    val coffee=coffee
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.coffee_view,container,false)
+
+        val coffeeText = view.findViewById<TextView>(R.id.coffee_title)
+        coffeeText.text=coffee.getName()
+
+        val priceText = view.findViewById<TextView>(R.id.coffee_price)
+        priceText.text=reformatNumber(coffee.getPrice())+" VNĐ"
+
+        val imageView = view.findViewById<ImageView>(R.id.coffee_image)
+        imageView.setImageResource(Functions.imageFromCoffee(requireContext(), coffee)) //đặt hình ảnh
+
         return view
     }
 }
