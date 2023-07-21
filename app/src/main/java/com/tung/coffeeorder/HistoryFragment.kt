07c342873c4897b9642f994d45412a23573efdf0
viettungvuong <viettungvuong@gmail.com>
2023-07-21 22:34:56 +0700

    Thêm 2 fragment cho 2 tab

---
+++ app/src/main/java/com/tung/coffeeorder/HistoryFragment.kt
@@ -0,0 +1,22 @@
+package com.tung.coffeeorder
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+
+
+class HistoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.history_fragment, container, false)
+    }
+
+}
