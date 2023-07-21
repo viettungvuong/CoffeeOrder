9c0d80d5951b04764666085fd48948c44db4c392
viettungvuong <viettungvuong@gmail.com>
2023-07-21 22:19:35 +0700

    Update OrderSuccess.kt

--- app/src/main/java/com/tung/coffeeorder/OrderSuccess.kt
+++ app/src/main/java/com/tung/coffeeorder/OrderSuccess.kt
@@ -2,11 +2,15 @@ package com.tung.coffeeorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 
 class OrderSuccess : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.order_success)
+
+        val trackOrderBtn = findViewById<MaterialButton>(R.id.trackBtn)
+        
     }
 }
