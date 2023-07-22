11f9f1b85a2ebb35d91e9c55c4b3c235ad8d7bbd
viettungvuong <viettungvuong@gmail.com>
2023-07-22 09:11:48 +0700

    Thêm adapter

--- app/src/main/java/com/tung/coffeeorder/HistoryFragment.kt
+++ app/src/main/java/com/tung/coffeeorder/HistoryFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tung.coffeeorder.AppController.Companion.orders
 
 // TODO: Rename parameter arguments, choose names that match
 
@@ -16,7 +19,12 @@ class HistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.history_fragment, container, false)
+        val view = inflater.inflate(R.layout.history_fragment, container, false)
+        val ordersRecycler = view.findViewById<RecyclerView>(R.id.historyRecycler)
+        ordersRecycler.layoutManager=LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
+        ordersRecycler.adapter=OrderAdapter(requireActivity(),orders)
+
+        return view
     }
 
 }
--- app/src/main/java/com/tung/coffeeorder/OngoingFragment.kt
+++ app/src/main/java/com/tung/coffeeorder/OngoingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class OngoingFragment : Fragment() {
 
@@ -13,7 +15,13 @@ class OngoingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.ongoing_fragment, container, false)
+        val view = inflater.inflate(R.layout.ongoing_fragment, container, false)
+        val ordersRecycler = view.findViewById<RecyclerView>(R.id.ongoingRecycler)
+        ordersRecycler.layoutManager=
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL,false)
+        ordersRecycler.adapter=OrderAdapter(requireActivity(), AppController.orders)
+
+        return view
     }
 
 }
