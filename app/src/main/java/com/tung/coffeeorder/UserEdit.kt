28901e2e080e09b5fc7f113506cc0cec6590d288
viettungvuong <viettungvuong@gmail.com>
2023-07-22 22:38:34 +0700

    Update UserEdit.kt

--- app/src/main/java/com/tung/coffeeorder/UserEdit.kt
+++ app/src/main/java/com/tung/coffeeorder/UserEdit.kt
@@ -18,11 +18,12 @@ class UserEdit : AppCompatActivity() {
     lateinit var userPhone: EditText
     lateinit var userAddress: EditText
 
-    var editMode=false
+    var editMode = false
     override fun onStart() {
         super.onStart()
-        this.currentuser=user
+        this.currentuser = user
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_activity)
@@ -32,77 +33,111 @@ class UserEdit : AppCompatActivity() {
         userPhone = findViewById<EditText>(R.id.userPhone)
         userAddress = findViewById<EditText>(R.id.userAddress)
 
-        userName.text= Editable.Factory.getInstance().newEditable(user.getname())
-        userEmail.text= Editable.Factory.getInstance().newEditable(user.getemail())
-        userPhone.text= Editable.Factory.getInstance().newEditable(user.getphoneNumber())
-        userAddress.text= Editable.Factory.getInstance().newEditable(user.getaddress())
+        userName.text = Editable.Factory.getInstance().newEditable(user.getname())
+        userEmail.text = Editable.Factory.getInstance().newEditable(user.getemail())
+        userPhone.text = Editable.Factory.getInstance().newEditable(user.getphoneNumber())
+        userAddress.text = Editable.Factory.getInstance().newEditable(user.getaddress())
 
         val nameBtn = findViewById<ImageButton>(R.id.mainProfileBtn)
         val emailBtn = findViewById<ImageButton>(R.id.mainEmailBtn)
         val phoneBtn = findViewById<ImageButton>(R.id.mainPhoneBtn)
         val addressBtn = findViewById<ImageButton>(R.id.mainAddressBtn)
 
-        nameBtn.setOnClickListener{
-            view->changeName(view)
+        nameBtn.setOnClickListener { view ->
+            changeName(view)
         }
 
-        emailBtn.setOnClickListener{
-            view->changeEmail(view)
+        emailBtn.setOnClickListener { view ->
+            changeEmail(view)
         }
 
-        phoneBtn.setOnClickListener{
-            view->changePhone(view)
+        phoneBtn.setOnClickListener { view ->
+            changePhone(view)
         }
 
-        addressBtn.setOnClickListener{
-            view->changeAddress(view)
+        addressBtn.setOnClickListener { view ->
+            changeAddress(view)
         }
 
     }
 
-    fun changeName(view: View){
-            editMode=true
-            userName.isEnabled=true
-            userEmail.isEnabled=false
-            userPhone.isEnabled=false
-            userAddress.isEnabled=false
+    fun changeName(view: View) {
+        editMode = true
+        userName.isEnabled = true
+        userEmail.isEnabled = false
+        userPhone.isEnabled = false
+        userAddress.isEnabled = false
+
+        val cancelButton = findViewById<ImageButton>(R.id.secondaryProfileBtn)
+        cancelButton.setImageResource(R.drawable.cancel)
+        cancelButton.setOnClickListener{
+            view->cancelEdit(view)
+        }
+
+        (view as ImageButton).setImageResource(R.drawable.save_change)
+    }
+
+    fun changeEmail(view: View) {
+
+        editMode = true
+        userEmail.isEnabled = true
+        userName.isEnabled = false
+        userPhone.isEnabled = false
+        userAddress.isEnabled = false
+
+        val cancelButton = findViewById<ImageButton>(R.id.secondaryEmailBtn)
+        cancelButton.setImageResource(R.drawable.cancel)
+        cancelButton.setOnClickListener{
+                view->cancelEdit(view)
+        }
+
+        (view as ImageButton).setImageResource(R.drawable.save_change)
 
     }
 
-    fun changeEmail(view: View){
+    fun changePhone(view: View) {
 
-        editMode=true
-        userEmail.isEnabled=true
-        userName.isEnabled=false
-        userPhone.isEnabled=false
-        userAddress.isEnabled=false
+        editMode = true
+        userPhone.isEnabled = true
+        userName.isEnabled = false
+        userEmail.isEnabled = false
+        userAddress.isEnabled = false
 
+        val cancelButton = findViewById<ImageButton>(R.id.secondaryPhoneBtn)
+        cancelButton.setImageResource(R.drawable.cancel)
+        cancelButton.setOnClickListener{
+                view->cancelEdit(view)
+        }
+
+        (view as ImageButton).setImageResource(R.drawable.save_change)
     }
 
-    fun changePhone(view: View){
+    fun changeAddress(view: View) {
 
-        editMode=true
-        userPhone.isEnabled=true
-        userName.isEnabled=false
-        userEmail.isEnabled=false
-        userAddress.isEnabled=false
+        editMode = true
+        userAddress.isEnabled = true
+        userName.isEnabled = false
+        userPhone.isEnabled = false
+        userEmail.isEnabled = false
+
+        val cancelButton = findViewById<ImageButton>(R.id.secondaryAddressBtn)
+        cancelButton.setImageResource(R.drawable.cancel)
+        cancelButton.setOnClickListener{
+                view->cancelEdit(view)
+        }
+
+        (view as ImageButton).setImageResource(R.drawable.save_change)
     }
 
-    fun changeAddress(view: View){
+    fun cancelEdit(view: View) {
 
-        editMode=true
-        userAddress.isEnabled=true
-        userName.isEnabled=false
-        userPhone.isEnabled=false
-        userEmail.isEnabled=false
+        editMode = false
+        userAddress.isEnabled = false
+        userName.isEnabled = false
+        userPhone.isEnabled = false
+        userEmail.isEnabled = false
+
+        view.setVisibility(View.INVISIBLE) //ẩn nút
     }
-
-    fun cancelEdit(view: View){
-
-        editMode=false
-        userAddress.isEnabled=false
-        userName.isEnabled=false
-        userPhone.isEnabled=false
-        userEmail.isEnabled=false}
 
 }
