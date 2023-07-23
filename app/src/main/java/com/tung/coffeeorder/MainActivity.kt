8421880ad188434bff12926676bd87d22e8a5843
viettungvuong <viettungvuong@gmail.com>
2023-07-24 00:22:48 +0700

    Update MainActivity.kt

--- app/src/main/java/com/tung/coffeeorder/MainActivity.kt
+++ app/src/main/java/com/tung/coffeeorder/MainActivity.kt
@@ -26,6 +26,15 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
+        val caller=intent?.getStringExtra("CallingActivity")
+        if (caller!=null){
+            if (caller=="OrderSuccess"){
+                val thirdMenuItem: MenuItem = findViewById<BottomNavigationView>(R.id.bottom_navigation).menu.getItem(2)
+                thirdMenuItem.isChecked = true //mở orders fragment
+                (this as FragmentActivity).supportFragmentManager.beginTransaction().replace(R.id.fragment,Orders()).commit() //hiện fragment lên
+            }
+        }
+        //khi mà quay lại actvitiy mà caller là OrderSuccess thì nó sẽ mở đúng tab
 
     }
 
@@ -44,19 +53,6 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().replace(R.id.fragment,Home()).commit() //hiện fragment Home đầu tiên
 
         bottomNavigationHandler=BottomNavigationHandler(this,bottomNavigationView) //handler bottom navigation view
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val caller=intent?.getStringExtra("CallingActivity")
-        if (caller!=null){
-            if (caller=="OrderSuccess"){
-                val thirdMenuItem: MenuItem = findViewById<BottomNavigationView>(R.id.bottom_navigation).menu.getItem(2)
-                thirdMenuItem.isChecked = true //mở orders fragment
-                (this as FragmentActivity).supportFragmentManager.beginTransaction().replace(R.id.fragment,Orders()).commit() //hiện fragment lên
-            }
-        }
 
     }
 
