966e8a4b78cc5e8a317c0ba64f2b4c82c39ee1c8
viettungvuong <viettungvuong@gmail.com>
2023-07-23 10:38:51 +0700

    tạo signup

---
+++ app/src/main/java/com/tung/coffeeorder/Signup.kt
@@ -0,0 +1,11 @@
+package com.tung.coffeeorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_activity)
+    }
+}
